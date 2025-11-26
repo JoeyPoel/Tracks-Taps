bbc2bb21ad8126f53a29ad42604b40284055d1fd
Joey van der Poel <Joeywognum@gmail.com>
2025-11-26 22:18:10 +0100

    Feat: Automatic theme selection based on device settings

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,28 +1,44 @@
-import { darkTheme, lightTheme } from '@/src/context/theme';
-import React, { createContext, ReactNode, useContext, useState } from 'react';
+import React, {createContext, ReactNode, useContext, useEffect, useState} from 'react';
+import { darkTheme, lightTheme } from './theme';
+import {ColorSchemeName, useColorScheme} from "react-native";
+import {JSX} from "react/jsx-runtime";
 
-type ThemeType = typeof lightTheme;
+type ThemeMode = "light" | "dark";
 
 interface ThemeContextProps {
-  theme: ThemeType;
-  toggleTheme: () => void;
+    mode: ThemeMode;
+    theme: typeof lightTheme;
+    toggleTheme: () => void;
+}
+
+const themes = {
+    light: lightTheme,
+    dark: darkTheme,
 }
 
 const ThemeContext = createContext<ThemeContextProps>({
-  theme: lightTheme,
-  toggleTheme: () => {},
+    mode: "light",
+    theme: lightTheme,
+    toggleTheme: (): void => {},
 });
 
-export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState(lightTheme);
 
-  const toggleTheme = () => setTheme(prev => (prev === lightTheme ? darkTheme : lightTheme));
+export const ThemeProvider = ({ children }: { children: ReactNode }): JSX.Element => {
+    const systemScheme: ColorSchemeName = useColorScheme(); // "light" | "dark"
+    const [mode, setMode] = useState<ThemeMode>(systemScheme === "dark" ? "dark" : "light");
 
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
+    useEffect((): void => {
+        setMode(systemScheme === "dark" ? "dark" : "light");
+    }, [systemScheme]);
+
+    const toggleTheme = (): void =>
+        setMode(prev => (prev === "light" ? "dark" : "light"));
+
+    return (
+        <ThemeContext.Provider value={{ mode, theme: themes[mode], toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
 };
 
-export const useTheme = () => useContext(ThemeContext);
+export const useTheme = (): ThemeContextProps => useContext(ThemeContext);
