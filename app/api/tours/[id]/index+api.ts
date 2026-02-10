2c4a10f11427adb94e4d81ac5e45f706dfa0da8b
Joey van der Poel <Joeywognum@gmail.com>
2026-02-10 11:25:58 +0100

    Fix: Able to update tour

--- app/api/tours/[id]/index+api.ts
+++ app/api/tours/[id]/index+api.ts
@@ -3,3 +3,7 @@ import { tourController } from '@/backend/controllers/tourController';
 export async function GET(request: Request, params: { id: string }) {
     return await tourController.getTourById(request, params);
 }
+
+export async function PUT(request: Request, params: { id: string }) {
+    return await tourController.updateTour(request, params);
+}
